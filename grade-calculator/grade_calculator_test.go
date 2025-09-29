e1ce77ef0acaffba337e364edfa051cd43db4fdd
NicholasThyssen <n.thyssen@ufl.edu>
2025-09-29 01:35:50 -0400

    This commit is made for the 100% code coverage.

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -34,6 +34,38 @@ func TestGetGradeB(t *testing.T) {
 	}
 }
 
+func TestGetGradeC(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 75, Assignment)
+	gradeCalculator.AddGrade("exam 1", 76, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 72, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeD(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 64, Assignment)
+	gradeCalculator.AddGrade("exam 1", 65, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 67, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
 func TestGetGradeF(t *testing.T) {
 	expected_value := "F"
 
@@ -48,4 +80,37 @@ func TestGetGradeF(t *testing.T) {
 	if expected_value != actual_value {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
 	}
+}
+
+func TestGetListSize(t *testing.T) {
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 50, Assignment)
+	gradeCalculator.AddGrade("exam 1", 40, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 30, Essay)
+
+	if len(gradeCalculator.assignments) != 1 {
+		t.Errorf("Expected 1 grade to be in assignments and instead got '%d'", len(gradeCalculator.assignments))
+	}
+	if len(gradeCalculator.exams) != 1 {
+		t.Errorf("Expected 1 grade to be in exams and instead got '%d'", len(gradeCalculator.exams))
+	}
+	if len(gradeCalculator.essays) != 1 {
+		t.Errorf("Expected 1 grade to be in essays and instead got '%d'", len(gradeCalculator.essays))
+	}
+
+}
+
+func TestGradeNames(t *testing.T) {
+	if Assignment.String() != "assignment" {
+		t.Errorf("Expected assignment as the String but instead got '%s'", Assignment.String())
+	}
+	if Exam.String() != "exam" {
+		t.Errorf("Expected exam as the String but instead got '%s'", Exam.String())
+	}
+	if Essay.String() != "essay" {
+		t.Errorf("Expected essay as the String but instead got '%s'", Essay.String())
+	}
+
 }
